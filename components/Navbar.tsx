8e1538f8cb94cfb631034180ac131b08b04c70d1
davion019029 <davion019029@gmail.com>
2025-04-23 11:11:07 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-shrink-0">
-            <span className="text-2xl font-bold">E-Commerce</span>
+            <span className="text-2xl font-bold">Stecu</span>
           </div>
 
           <div className="hidden space-x-4 md:flex">
